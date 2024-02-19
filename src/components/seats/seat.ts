27d4587031b92a2e454c5982257cb6884ce3ef33
jimmyhealer <yahing6066@gmail.com>
2024-02-19 12:23:49 +0800

    feat: seat map change style (version 1)

--- src/components/seats/seat.ts
+++ src/components/seats/seat.ts
@@ -17,26 +17,12 @@ export class Seat extends Group {
     y: number,
     rotation: number,
     // TODO: text content should be the seat number
-    onlyTable: boolean = false,
     text: string = ''
   ) {
-    // Chair, Text 14x12
-    // Table 28x19
     const children = [
-      new Table(0, 12, 28, 19, '#fab469'),
+      new Chair(0, 0, 38, 38),
+      new Table(0, 25, 38, 38, text),
     ]
-
-    if (!onlyTable) {
-      text = DrawUntil.seat_id_gen()
-      children.push(
-        new Chair(7, 0, 14, 12),
-        new Text(0, 12, 28, 19, text)
-      )
-    }
-    else {
-      console.log('Seat', text)
-      console.log(children)
-    }
 
     super(x, y, rotation, children, 'seat')
   }
