e87eb51e097655810cae174c71fcd3841f80d6a3
jimmyhealer <yahing6066@gmail.com>
2024-03-20 00:17:48 +0800

    feat: add static inject method and format code

--- src/DependencyContainer.ts
+++ src/DependencyContainer.ts
@@ -1,25 +1,29 @@
 export default class DependencyContainer {
-  private static instance: DependencyContainer;
-  private dependencies: Map<string, any> = new Map();
+  private static instance: DependencyContainer
+  private dependencies: Map<string, any> = new Map()
 
   private constructor() {}
 
   public static getInstance(): DependencyContainer {
     if (!DependencyContainer.instance) {
-      DependencyContainer.instance = new DependencyContainer();
+      DependencyContainer.instance = new DependencyContainer()
     }
-    return DependencyContainer.instance;
+    return DependencyContainer.instance
+  }
+
+  public static inject<T>(key: string): T {
+    return DependencyContainer.getInstance().resolve<T>(key)
   }
 
   public register(key: string, instance: any): void {
-    this.dependencies.set(key, new instance());
+    this.dependencies.set(key, new instance())
   }
 
   public resolve<T>(key: string): T {
-    const dependency = this.dependencies.get(key);
+    const dependency = this.dependencies.get(key)
     if (dependency) {
-      return dependency as T;
+      return dependency as T
     }
-    throw new Error(`Dependency ${key} not found.`);
+    throw new Error(`Dependency ${key} not found.`)
   }
 }
