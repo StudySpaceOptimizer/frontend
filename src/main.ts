fae539af93b832d7ba83d017149aae084b2490c7
jimmyhealer <yahing6066@gmail.com>
2024-03-20 00:12:11 +0800

    feat: add new DI method

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
+import * as api from './api'
+import DependencyContainer from '@/DependencyContainer'
 
 import VueKonva from 'vue-konva'
 
@@ -13,6 +15,12 @@ const app = createApp(App)
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
+
+// Dependency Injection
+const container = DependencyContainer.getInstance()
+container.register(api.API_SERVICE.USER, api.MockUser)
+container.register(api.API_SERVICE.SEAT, api.PouchDbSeat)
+container.register(api.API_SERVICE.RESERVE, api.MockReserve)
 
 app.use(pinia)
 app.use(router)
