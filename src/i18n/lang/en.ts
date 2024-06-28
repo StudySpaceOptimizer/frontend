36c4c1dfae1a6ce07343e068ff5f6e062489d7e1
jimmyhealer <yahing6066@gmail.com>
2024-06-28 12:49:15 +0800

    feat: update i18n

--- src/i18n/lang/en.ts
+++ src/i18n/lang/en.ts
@@ -3,6 +3,40 @@ export default {
     signIn: 'Sign In',
     signOut: 'Sign Out',
     profile: 'Profile',
-    reservation: 'Reservation'
-  }
+    reservation: 'Reservation',
+    title: 'Hi, {name}!'
+  },
+  signInModal: {
+    title: 'Sign In',
+    emailLabel: 'Email',
+    emailPlaceholder: 'Please enter your email',
+    passwordLabel: 'Password',
+    passwordPlaceholder: 'Please enter your password',
+    signUp: 'OutSider Sign Up',
+    signIn: 'Sign In',
+  },
+  signUpModal: {
+    title: 'Outsider Sign Up',
+    name: 'Name',
+    telphone: 'Phone',
+    idCard: 'ID Card',
+    email: 'Email',
+    signUp: 'Sign Up',
+    namePlaceholder: 'Please enter your name',
+    telphonePlaceholder: 'Please enter your phone number',
+    idCardPlaceholder: 'Please enter your ID card',
+    emailPlaceholder: 'Please enter your email',
+  },
+  seat: {
+    StairDoor: 'Stair Door',
+    MainDoor: 'Main Door',
+    MenToilet: 'Men\'s Toilet',
+    Elevator: 'Elevator',
+  },
+  available: 'Available',
+  booked: 'Booked',
+  partiallyBooked: 'Partially Booked',
+  unavailable: 'Unavailable',
+
+  filter: 'Start Filtering',
 }
--- src/i18n/lang/zh-tw.ts
+++ src/i18n/lang/zh-tw.ts
@@ -3,6 +3,40 @@ export default {
     signIn: '登入',
     signOut: '登出',
     profile: '個人資料',
-    reservation: '預約紀錄'
-  }
+    reservation: '預約紀錄',
+    title: '嗨，{name}！'
+  },
+  signInModal: {
+    title: '登入',
+    emailLabel: '信箱',
+    emailPlaceholder: '請輸入信箱',
+    passwordLabel: '密碼',
+    passwordPlaceholder: '請輸入密碼',
+    signUp: '校外人士註冊',
+    signIn: '登入',
+  },
+  signUpModal: {
+    title: '校外人士註冊',
+    name: '姓名',
+    telphone: '手機',
+    idCard: '身分證字號',
+    email: '信箱',
+    signUp: '註冊',
+    namePlaceholder: '請輸入姓名',
+    telphonePlaceholder: '請輸入手機號碼',
+    idCardPlaceholder: '請輸入身分證字號',
+    emailPlaceholder: '請輸入信箱',
+  },
+  seat: {
+    StairDoor: '樓梯門',
+    MainDoor: '大門',
+    MenToilet: '男廁',
+    Elevator: '電梯',
+  },
+  available: '可預約',
+  booked: '已預約',
+  partiallyBooked: '部分時段已預約',
+  unavailable: '該座位不可預約',
+
+  filter: '開始篩選',
 }
